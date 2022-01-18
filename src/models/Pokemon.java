23bc158e8fcacb483d82e02ea4a3ef1f5ac75566
Elisa <aerodriguez4@gmail.com>
2022-01-18 18:47:46 +0100

    Pokemon class created

--- src/models/Pokemon.java
+++ src/models/Pokemon.java
@@ -2,4 +2,100 @@ package models;
 
 public class Pokemon {
 
+	//Atributos
+	private int id;
+	private String nombre;
+	private String tipo1;
+	private String tipo2;
+	private double altura;
+	private double peso;
+	private String categoria;
+	private String habilidad;
+	
+	//Constructor
+	public Pokemon(int id, String nombre, String tipo1, String tipo2, double altura, double peso, String categoria,
+			String habilidad) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.tipo1 = tipo1;
+		this.tipo2 = tipo2;
+		this.altura = altura;
+		this.peso = peso;
+		this.categoria = categoria;
+		this.habilidad = habilidad;
+	}
+
+	//Getters y setters
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getTipo1() {
+		return tipo1;
+	}
+
+	public void setTipo1(String tipo1) {
+		this.tipo1 = tipo1;
+	}
+
+	public String getTipo2() {
+		return tipo2;
+	}
+
+	public void setTipo2(String tipo2) {
+		this.tipo2 = tipo2;
+	}
+
+	public double getAltura() {
+		return altura;
+	}
+
+	public void setAltura(double altura) {
+		this.altura = altura;
+	}
+
+	public double getPeso() {
+		return peso;
+	}
+
+	public void setPeso(double peso) {
+		this.peso = peso;
+	}
+
+	public String getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+
+	public String getHabilidad() {
+		return habilidad;
+	}
+
+	public void setHabilidad(String habilidad) {
+		this.habilidad = habilidad;
+	}
+
+	//To String
+	@Override
+	public String toString() {
+		return "Pokemon [id=" + id + ", nombre=" + nombre + ", tipo1=" + tipo1 + ", tipo2=" + tipo2 + ", altura="
+				+ altura + ", peso=" + peso + ", categoria=" + categoria + ", habilidad=" + habilidad + "]";
+	}
+	
 }
