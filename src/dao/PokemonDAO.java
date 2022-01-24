b80f6889f87981001d540c222b54a69c5750febc
Elisa <aerodriguez4@gmail.com>
2022-01-24 20:53:23 +0100

    Pokemon DAO ready to query in DB

--- src/dao/PokemonDAO.java
+++ src/dao/PokemonDAO.java
@@ -5,30 +5,58 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 
 import models.Pokemon;
+import views.PokedexView;
 
-public class PokemonDAO {
+public class PokemonDAO extends AbstractDAO {
 
-	final String DB_URL = "jdbc:mysql://localhost/pokedex";
-	final String USER = "Rawdevil";
-	final String PASS = "Put0user22";
+	private PokedexView pokedex;
 	
-
-	public void consulta(Pokemon pokemon) {
-		final String QUERY = "SELECT * FROM pokemon";
+	public Pokemon first() {
+		final String QUERY = "SELECT * FROM pokemon LIMIT 1";
 		try {
-			Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
-			Statement stmt = conn.createStatement();
 			ResultSet rs = stmt.executeQuery(QUERY);
 			while (rs.next()) {
-				// Display values
-				System.out.print("Nombre: " + rs.getString("nombre"));
-				System.out.println(", Tipo 1: " + rs.getString("tipo1"));
+				int id = rs.getInt("id");
+				String nombre = rs.getString("nombre");
+				String tipo1 = rs.getString("tipo1");
+				String tipo2 = rs.getString("tipo2");
+				double altura = rs.getDouble("altura");
+				double peso = rs.getDouble("peso");
+				String categoria = rs.getString("categoria");
+				String habilidad = rs.getString("habilidad");
+				Pokemon p = new Pokemon(id, nombre, tipo1, tipo2, altura, peso, categoria, habilidad);
+				return p;
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
+		return null;
 
+	}
+	
+	public ArrayList<Pokemon> getAll() {
+		final String QUERY = "SELECT * FROM pokemon";
+		var pokemons = new ArrayList<Pokemon>();
+		try {
+			ResultSet rs = stmt.executeQuery(QUERY);
+			while (rs.next()) {
+				var id = rs.getInt("id");
+				var nombre = rs.getString("nombre");
+				var tipo1 = rs.getString("tipo1");
+				var tipo2 = rs.getString("tipo2");
+				var altura = rs.getDouble("altura");
+				var peso = rs.getDouble("peso");
+				var categoria = rs.getString("categoria");
+				var habilidad = rs.getString("habilidad");
+				Pokemon p = new Pokemon(id, nombre, tipo1, tipo2, altura, peso, categoria, habilidad);
+				pokemons.add(p);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return pokemons;
 	}
 }
