e97a264f23f0ae737a20680c75db6a6bb84a5109
Elisa <aerodriguez4@gmail.com>
2022-02-26 13:13:03 +0100

    PokeCreatorView documentation done

--- src/views/PokeCreatorView.java
+++ src/views/PokeCreatorView.java
@@ -26,56 +26,52 @@ import javax.swing.JComboBox;
 
 public class PokeCreatorView {
 
+	// Marco
 	private JFrame frame;
+	// Etiquetas de texto e imágenes
 	private JLabel lblImgBackground;
+	private JLabel lblMeters;
+	private JLabel lblKilos;
+	private JLabel lblHabilidad;
+	private JLabel lblAlmohadilla;
+	// Imagen de fondo
 	private Image background;
+	// Campos de texto y menús de selección desplegables
 	private JTextField tfId;
 	private JTextField tfNombre;
 	private JTextField tfCategoria;
 	private JTextField tfHeight;
 	private JTextField tfWeight;
 	private JTextField tfHabilidad;
-	private JLabel lblMeters;
-	private JLabel lblKilos;
-	private JLabel lblHabilidad;
-	// private JPanel lblCrossHoriz;
-	// private JPanel lblCrossVertic;
+	private JComboBox<String> cbTipo1;
+	private JComboBox<String> cbTipo2;
+	// Botones
 	private JButton btnGuardar;
 	private JButton btnCancelar;
+	// Importaciones de otras clases necesarias
 	private PokemonDAO pokemonDAO;
 	private Pokemon pokemon;
-	private TipoDAO	tipoDAO;
-	private JLabel lblAlmohadilla;
+	private TipoDAO tipoDAO;
+	// ArrayList de Pokémon y Tipo + índice para la Pokédex
 	private ArrayList<Pokemon> pokemons;
 	private ArrayList<Tipo> tipos;
 	private int index;
-	private JComboBox<String> cbTipo1;
-	private JComboBox<String> cbTipo2;
 
 	/**
-	 * Create the application.
+	 * Creación de la aplicación. Reservamos memoria a un nuevo PokemonDAO y TipoDAO
+	 * y rellenamos los ArrayLists con Pokémon y los tipos de Pokémon
 	 */
 	public PokeCreatorView() {
-		// Imprimiremos el primer Pokemon con el índice inicializado a 0 (si en la BD
-		// hay mínimo 1 Pokemon almacenado):
+		/*
+		 * Imprimiremos el primer Pokemon con el índice inicializado a 0 (si en la BD
+		 * hay mínimo 1 Pokemon almacenado)
+		 */
 		this.pokemonDAO = new PokemonDAO();
 		this.tipoDAO = new TipoDAO();
 		this.pokemons = pokemonDAO.getAll();
-		this.tipos= tipoDAO.getAll(); 
+		this.tipos = tipoDAO.getAll();
 		initialize();
-		
 		index = 0;
-		
-		// Para que se vea la imagen de fondo tengo que dejarlo aquí, no puedo meterlo
-		// en el método configureUIComponents():
-		lblImgBackground = new JLabel("fondo");
-		lblImgBackground.setBackground(Color.RED);
-		lblImgBackground.setFont(new Font("Consolas", Font.PLAIN, 16));
-		lblImgBackground.setBounds(0, -23, 766, 445);
-		frame.getContentPane().add(lblImgBackground);
-		lblImgBackground.setIcon(new ImageIcon(background));
-		lblImgBackground.setSize(750, 500);
-
 	}
 
 	/**
@@ -95,22 +91,8 @@ public class PokeCreatorView {
 		background = new ImageIcon(this.getClass().getResource("/pokedex_entry.jpg")).getImage();
 		frame.setBounds(100, 100, 713, 487);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		btnCancelar = new JButton("CANCELAR");
-		btnCancelar.setFont(new Font("Courier New", Font.BOLD, 18));
-		btnCancelar.setBounds(403, 347, 209, 35);
-		frame.getContentPane().add(btnCancelar);
-		
-		cbTipo2 = new JComboBox<String>();
-		cbTipo2.setBounds(525, 143, 113, 35);
-		frame.getContentPane().add(cbTipo2);
-		
-		cbTipo1 = new JComboBox<String>();
-		cbTipo1.setBounds(402, 143, 113, 35);
-		frame.getContentPane().add(cbTipo1);
-		fillTipos();
-		
-		// Info Pokemon
+
+		// Info Pokemon (etiquetas de texto, campos de texto y menús desplegables)
 		tfId = new JTextField();
 		tfId.setOpaque(false);
 		tfId.setHorizontalAlignment(SwingConstants.LEFT);
@@ -189,7 +171,21 @@ public class PokeCreatorView {
 		frame.getContentPane().add(tfNombre);
 		tfNombre.setColumns(10);
 
+		cbTipo2 = new JComboBox<String>();
+		cbTipo2.setBounds(525, 143, 113, 35);
+		frame.getContentPane().add(cbTipo2);
+
+		cbTipo1 = new JComboBox<String>();
+		cbTipo1.setBounds(402, 143, 113, 35);
+		frame.getContentPane().add(cbTipo1);
+		fillTipos();
+
 		// Botones y otros detalles
+		btnCancelar = new JButton("CANCELAR");
+		btnCancelar.setFont(new Font("Courier New", Font.BOLD, 18));
+		btnCancelar.setBounds(403, 347, 209, 35);
+		frame.getContentPane().add(btnCancelar);
+
 		btnGuardar = new JButton("GUARDAR");
 		btnGuardar.setVisible(true);
 		btnGuardar.setMargin(new Insets(1, 1, 1, 1));
@@ -205,39 +201,68 @@ public class PokeCreatorView {
 		lblAlmohadilla.setBounds(576, 55, 54, 43);
 		frame.getContentPane().add(lblAlmohadilla);
 
+		lblImgBackground = new JLabel("fondo");
+		lblImgBackground.setBackground(Color.RED);
+		lblImgBackground.setFont(new Font("Consolas", Font.PLAIN, 16));
+		lblImgBackground.setBounds(0, -23, 766, 445);
+		frame.getContentPane().add(lblImgBackground);
+		lblImgBackground.setIcon(new ImageIcon(background));
+		lblImgBackground.setSize(750, 500);
+
 	}
 
 	private void configureUIListeners() {
+		/*
+		 * El botón Guardar guarda el nuevo Pokémon en la BD
+		 */
 		btnGuardar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-			createPokemon();
-			JOptionPane.showMessageDialog(btnGuardar, "Nuevo Pokémon creado.");
-			frame.dispose();
-			new PokedexView();
+				createPokemon();
+				JOptionPane.showMessageDialog(btnGuardar, "Nuevo Pokémon creado.");
+				frame.dispose();
+				new PokedexView();
 			}
 		});
-		
+
+		/*
+		 * El botón Cancelar elimina la vista y crea una nueva de la Pokédex.
+		 */
 		btnCancelar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-			frame.dispose();
-			new PokedexView();
+				frame.dispose();
+				new PokedexView();
 			}
 		});
 
 	}
 
+	/*
+	 * Método que asigna un índice único al nuevo Pokémon, crea una nueva instancia
+	 * de la clase Pokemon, la introduce en la lista de Pokémon de la vista y por
+	 * último introduce los datos del Pokémon en la BD. Se llama al método al pulsar
+	 * el botón Guardar
+	 */
 	private void createPokemon() {
-		//El id es el del último Pokemon guardado + 1
-		int lastIndexArray = pokemons.size()-1;
-		int newPokemonId = (pokemons.get(lastIndexArray).getId()+1);
+		// El id del nuevo Pokémon es el del último Pokémon + 1
+		int lastIndexArray = pokemons.size() - 1;
+		int newPokemonId = (pokemons.get(lastIndexArray).getId() + 1);
 		pokemon = new Pokemon(newPokemonId, null, null, null, index, index, null, null);
 		setInfoPokemon(pokemon);
 		pokemons.add(pokemon);
 		pokemonDAO.insert(pokemons.get(pokemons.lastIndexOf(pokemon)));
 	}
 
+	/**
+	 * Método que recoge la info de los campos de texto y de los menús desplegables
+	 * y los asigna a una instancia de la clase Pokémon. Se llama al método en el
+	 * método createPokemon().
+	 * 
+	 * @param pokemon - Instancia de la clase Pokémon que almacenará los datos
+	 *                introducidos en los campos de texto y ComboBoxes
+	 */
 	private void setInfoPokemon(Pokemon pokemon) {
-		if (tfNombre.getText().isEmpty() || tfHeight.getText().isEmpty() || tfWeight.getText().isEmpty() || tfCategoria.getText().isEmpty() || tfHabilidad.getText().isEmpty()) {
+		if (tfNombre.getText().isEmpty() || tfHeight.getText().isEmpty() || tfWeight.getText().isEmpty()
+				|| tfCategoria.getText().isEmpty() || tfHabilidad.getText().isEmpty()) {
 			JOptionPane.showMessageDialog(btnGuardar, "Revisa los campos.");
 		} else {
 			try {
@@ -246,17 +271,23 @@ public class PokeCreatorView {
 				pokemon.setPeso(Double.parseDouble(tfWeight.getText()));
 				pokemon.setCategoria(tfCategoria.getText());
 				pokemon.setHabilidad(tfHabilidad.getText());
-				//Obtenemos el tipo de Pokemon del ComboBox1 y del 2(si se ha seleccionado alguna opción que no sea 0)
+				// Obtenemos el tipo de Pokemon del ComboBox1 y del 2(si se ha seleccionado
+				// alguna opción que no sea 0)
 				pokemon.setTipoUno(tipos.get(cbTipo1.getSelectedIndex()).getNombreTipo());
-				if (cbTipo2.getSelectedIndex()!=0) {
-					pokemon.setTipoDos(tipos.get(cbTipo2.getSelectedIndex()+1).getNombreTipo());
+				if (cbTipo2.getSelectedIndex() != 0) {
+					pokemon.setTipoDos(tipos.get(cbTipo2.getSelectedIndex() - 1).getNombreTipo());
 				}
 			} catch (Exception e) {
 				JOptionPane.showMessageDialog(btnGuardar, "Algún campo no es correcto");
 			}
 		}
-		
+
 	}
+
+	/*
+	 * Método que introduce la lista de Tipos en los ComboBoxes o menús
+	 * desplegables.
+	 */
 	private void fillTipos() {
 		ArrayList<Tipo> tipos = tipoDAO.getAll();
 		cbTipo2.addItem("");
@@ -264,6 +295,6 @@ public class PokeCreatorView {
 			cbTipo1.addItem(t.getNombreTipo());
 			cbTipo2.addItem(t.getNombreTipo());
 		}
-		
+
 	}
 }
